e3a6894080692256d3b5a5aa2c6cf1eb4ea1ff4b
Raivals <103427644+Raivals@users.noreply.github.com>
2023-05-26 19:09:32 +0200

    Commit init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/* Main part of the App*/
+import React from "react"
+import Hero from "./components/Hero"
+import Demo from "./components/Demo"
+
+/* style */
+import "./App.css"
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+          
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
